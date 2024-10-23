71551fae06f58d4f8f233a120c5e91a11cafba1e
vitorialuz229 <vitorialuz831@gmail.com>
2024-10-23 15:39:11 -0300

    :sparkles: feat: Criando o objetos dos personagens

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const readline = require("readline");
+
+// Definindo os personagens
+const personagens = [
+  {
+    nome: "Mario",
+    velocidade: 4,
+    manobrabilidade: 3,
+    poder: 3,
+  },
+  { 
+    nome: "Peach", 
+    velocidade: 3, 
+    manobrabilidade: 4, 
+    poder: 2 
+  },
+  { 
+    nome: "Yoshi", 
+    velocidade: 2, 
+    manobrabilidade: 4, 
+    poder: 3 
+  },
+  { 
+    nome: "Bowser", 
+    velocidade: 5, 
+    manobrabilidade: 2, 
+    poder: 5 
+  },
+  { 
+    nome: "Luigi", 
+    velocidade: 3, 
+    manobrabilidade: 4, 
+    poder: 4 
+  },
+  { 
+    nome: "Donkey Kong", 
+    velocidade: 2, 
+    manobrabilidade: 2, 
+    poder: 5 
+  },
+];
